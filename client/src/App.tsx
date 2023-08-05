801dbb47e4a419c12627521816d82e6e04ebabd1
jannyshim <29jannyy@gmail.com>
2023-08-06 00:35:13 +0900

    feat: 뒤로가기 버튼 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,35 @@
 import React from "react";
 import ReservationPage from "./page/ReservationPage";
 import StationList from "./page/StationList";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 import SearchPage from "./page/SearchPage";
 import StationArrivalInfoPage from "./page/StationArrivalInfoPage";
+import OnboardingPage from "./page/OnboardingPage";
+
+const MyBackButton = () => {
+  const navigate = useNavigate();
+  const onClickBtn = () => {
+    navigate(-1);
+  };
+
+  // 첫페이지일때는 뒤로가기 버튼이 없음
+  if (window.location.pathname === "/") {
+    return null;
+  }
+
+  return <button onClick={onClickBtn}>뒤로가기</button>;
+};
 
 const App = () => {
   return (
     <BrowserRouter>
+      <MyBackButton />
       <Routes>
         <Route path="/" element={<SearchPage />} />
         <Route
@@ -16,13 +38,16 @@ const App = () => {
         />
         <Route path="/bus-stops" element={<StationList />} />
         <Route path="/reservation-page" element={<ReservationPage />} />
+        <Route path="/onboarding-page" element={<OnboardingPage />} />
       </Routes>
       <nav>
         <ul>
           <li>
             <Link to="/">처음으로</Link>
           </li>
-          <li>예약현황</li>
+          <li>
+            <Link to="/reservation-page">예약현황</Link>
+          </li>
         </ul>
       </nav>
     </BrowserRouter>
